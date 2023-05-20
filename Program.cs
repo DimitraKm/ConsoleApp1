72292cc6d9b2c257880b89c39af3ac11e6f71b25
DimitraKm <d.kontaki13@gmail.com>
2023-05-20 11:54:45 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            goblini g1 = new goblini("Asparuh", 16543, 16, 4);
+            goblini g2 = new goblini("Galena", 83743, 15, 6);
+            goblini g3 = new goblini("Stefka", 47293, 16, 5);
+
+            Console.WriteLine(g1.Name + " " + g1.ID + " " + g1.Age + " " + g1.Grade);
+            Console.WriteLine(g2.Name + " " + g2.ID + " " + g2.Age + " " + g2.Grade);
+            Console.WriteLine(g3.Name + " " + g3.ID + " " + g3.Age + " " + g3.Grade);
+
+            if (g1.Grade > g2.Grade && g1.Grade > g3.Grade)
+            {
+                Console.WriteLine(g1.Name + " " + g1.ID);
+            }
+            if (g2.Grade > g1.Grade && g2.Grade > g3.Grade)
+            {
+                Console.WriteLine(g2.Name + " " + g2.ID);
+            }
+            if (g3.Grade > g1.Grade && g3.Grade > g2.Grade)
+            {
+                Console.WriteLine(g3.Name + " " + g3.ID);
+            }
+        }
+    }
+}
---
+++ game.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class goblini
+    {
+
+        private string name;
+        private int id;
+        private int age;
+        private int grade;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public int Grade
+        {
+            get { return grade; }
+            set { grade = value; }
+        }
+
+
+        public goblini(string name, int id, int age, int grade)
+        {
+            this.name = name;
+            this.id = id;
+            this.age = age;
+            this.grade = grade;
+        }
+       
+
+    }
+}
